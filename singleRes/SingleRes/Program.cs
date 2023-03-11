aa9438df41cb1b6075712a3444e3cb538c7ca45c
luong228 <90311663+luong228@users.noreply.github.com>
2023-03-11 14:31:29 +0700

    Single Responsibility Principle

---
+++ singleRes/SingleRes/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace SingleRes
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public static Journal Load(string filename)
+        {
+            return new Journal();
+        }
+
+        public void Load(Uri uri)
+        {
+
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a dug");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"c:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+        }
+    }
+}
