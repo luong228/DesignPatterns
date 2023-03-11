6ebe6b10e3b57d77536009667836147ddc229022
luong228 <90311663+luong228@users.noreply.github.com>
2023-03-11 16:10:49 +0700

    Lisvkov Substitution Principle

---
+++ LiskovSub/LiskovSub/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace LiskovSub
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Console.WriteLine("rectangle");
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set => base.Width = base.Height = value;
+        }
+
+        public override int Height
+        {
+            set => base.Height = base.Width = value;
+        }
+    }
+    public class Demo
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Rectangle sq = new Square();
+            sq.Height = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
