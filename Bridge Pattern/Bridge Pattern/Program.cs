6e7f594f7025930fb30895fca06156a4dafc23c2
luong228 <90311663+luong228@users.noreply.github.com>
2023-05-06 11:24:06 +0700

    Bridge Pattern

---
+++ Bridge Pattern/Bridge Pattern/Program.cs
@@ -0,0 +1,69 @@
+﻿using Autofac;
+
+public interface IRenderer
+{
+    void RenderCircle(float radius);
+}
+
+public class VectorRenderer : IRenderer
+{
+    public void RenderCircle(float radius)
+    {
+        Console.WriteLine($"Drawing a circle of radius {radius}");
+    }
+
+}
+
+public class RasterRenderer : IRenderer
+{
+    public void RenderCircle(float radius)
+    {
+        Console.WriteLine($"Drawing pixels for circle with radius {radius}");
+    }
+}
+
+public abstract class Shape
+{
+    protected IRenderer renderer;
+
+    protected Shape(IRenderer renderer)
+    {
+        this.renderer = renderer;
+    }
+
+    public abstract void Draw();
+    public abstract void Resize(float factor);
+}
+
+public class Circle : Shape
+{
+
+    private float radius;
+
+    public Circle(IRenderer renderer, float radius) : base(renderer)
+    {
+        this.radius = radius;
+    }
+    public override void Draw()
+    {
+        renderer.RenderCircle(radius);
+    }
+
+    public override void Resize(float factor)
+    {
+        radius *= factor;
+    }
+}
+class Demo
+{
+    static void Main(string[] args)
+    {
+        var cb = new ContainerBuilder();
+        IRenderer renderer = new RasterRenderer();
+        var circle = new Circle(renderer, 5);
+
+        circle.Draw();
+        circle.Resize(2);
+        circle.Draw();
+    }
+}
